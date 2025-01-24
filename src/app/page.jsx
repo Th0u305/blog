bae52e72afec25ee7baa367cb8d9d4f58cbf33dd
Th0u305 <ahnaf4411@gmail.com>
2025-01-24 23:22:09 +0600

    good boy

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,25 @@
-import Link from "next/link";
+import { Link } from "lucide-react";
 
 const Home = async () => {
-  const data = await fetch('https://jsonplaceholder.typicode.com/posts')
-  const posts = await data.json()
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await data.json();
 
   return (
     <div>
       <h1 className="text-2xl font-medium">All Posts: {posts.length}</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-4">
         {posts.map((post, index) => (
-          <Link className="block hover:text-cyan-500" href={`/blog/${post?.id}`} key={post.id}>{index + 1}. {post.title}</Link>
+          <div
+            className="card bg-neutral text-neutral-content w-80"
+            key={index}
+          >
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{post?.title}</h2>
+              <div className="card-actions justify-end">
+               <a className="btn btn-primary" href={`/blog/${post.id}`}>View more</a>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,47 +1,63 @@
 import Link from "next/link";
-import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 import getUser from "@/lib/getUser";
 import NavbarSidebar from "./NavbarSidebar";
 
 const Navbar = async () => {
-    const { email } = await getUser() || {};
+  const { email, picture } = (await getUser()) || {};
+  // console.log(await getUser());
 
-    return (
-        <nav className="flex justify-between items-center py-4 bg-gray-100 sticky top-0 z-50">
-            <div className="w-10/12 mx-auto flex justify-between items-center">
-                {/* Logo */}
-                <div className="inline-flex items-center">
-                    <span className="flex h-11 w-11 items-center justify-center rounded-md text-heading-6 font-semibold text-2xl bg-black text-white">
-                        T
-                    </span>
-                </div>
+  return (
+    <nav className="flex justify-between items-center py-4 bg-gray-100 sticky top-0 z-50">
+      <div className="w-10/12 mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <div className="inline-flex items-center">
+          <span className="flex h-11 w-auto p-5 items-center justify-center rounded-md text-heading-6 font-semibold text-2xl bg-black text-white">
+            Blogs
+          </span>
+        </div>
 
-                {/* Desktop Navigation */}
-                <div className="hidden md:flex items-center space-x-4">
-                    <ul className="flex font-medium justify-center space-x-4 pr-4 border-r-2 border-gray-300">
-                        <li className="hover:underline">
-                            <Link href="/">Home</Link>
-                        </li>
-                        <li className="hover:underline">
-                            <Link href="/profile">Profile</Link>
-                        </li>
-                    </ul>
-
-                    {email ? (
-                        <LogoutLink className="btn btn-neutral">Log out</LogoutLink>
-                    ) : (
-                        <>
-                            <LoginLink className="btn btn-neutral">Sign in</LoginLink>
-                            <RegisterLink className="btn btn-neutral">Sign up</RegisterLink>
-                        </>
-                    )}
-                </div>
-
-                {/* Mobile Sidebar */}
-                <NavbarSidebar email={email} />
+        {/* Desktop Navigation */}
+        <div className="hidden md:flex items-center space-x-4">
+          <ul className="flex font-medium items-center justify-center space-x-4 pr-4 border-r-2 border-gray-300">
+            <li className="hover:underline">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/profile">Profile</Link>
+            </li>
+            <div className="avatar online placeholder">
+              <div className="bg-neutral text-neutral-content w-12 rounded-full">
+                <img
+                  src={
+                    picture ||
+                    "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  }
+                  alt=""
+                />
+              </div>
             </div>
-        </nav>
-    );
+          </ul>
+
+          {email ? (
+            <LogoutLink className="btn btn-neutral">Log out</LogoutLink>
+          ) : (
+            <>
+              <LoginLink className="btn btn-neutral">Sign in</LoginLink>
+              <RegisterLink className="btn btn-neutral">Sign up</RegisterLink>
+            </>
+          )}
+        </div>
+
+        {/* Mobile Sidebar */}
+        <NavbarSidebar email={email} />
+      </div>
+    </nav>
+  );
 };
 Navbar.displayName = "MyComponent";
 export default Navbar;
